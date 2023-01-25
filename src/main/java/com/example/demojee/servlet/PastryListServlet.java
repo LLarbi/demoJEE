5f0fb3a0a6137afb0c3c19315f709788deb12e42
LLarbi <larbi_lachgare@hotmail.fr>
2023-01-25 12:16:47 +0100

    ajout de la servlet pour recuperer toutes les patisseries

---
+++ src/main/java/com/example/demojee/servlet/PastryListServlet.java
@@ -0,0 +1,29 @@
+package com.example.demojee.servlet;
+
+import com.example.demojee.dao.DaoFactory;
+import com.example.demojee.dao.PastryDao;
+import com.example.demojee.dao.entity.Pastry;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/pastries")
+public class PastryListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PastryDao dao = DaoFactory.getPastryDao();
+        List<Pastry> pastryList = dao.getAll();
+
+        req.setAttribute("pastries", pastryList);
+
+        req.getRequestDispatcher("/WEB-INF/pastry-list.jsp").forward(req, resp);
+    }
+
+
+}
